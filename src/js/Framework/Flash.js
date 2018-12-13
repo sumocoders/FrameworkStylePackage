593d26648bc511ed272265add2f8d7e0d13e12c7
Verstraete Kevin <kevin@sumocoders.be>
2018-12-13 13:12:22 +0100

    Remove container from alert
    The container gives the closing button wrapping div wrong width and height values causing the button to disappear.

--- src/js/Framework/Flash.js
+++ src/js/Framework/Flash.js
@@ -6,14 +6,12 @@ export class Flash {
     let alertId = Date.now()
 
     $('.main-header').append(
-        `<div class="alert alert-${type} alert-dismissible notification" role="status" data-alert-id="${alertId}">
-        <div class="container">
-          <a class="close" data-dismiss="alert" title="${locale.lbl('core.interface.close')}">
-            <i class="fa fa-close"></i>
-            <span class="hide">${locale.lbl('core.interface.close')}</span>
-          </a> ${message}
-        </div>
-      </div>`
+      `<div class="alert alert-${type} alert-dismissible notification" role="status" data-alert-id="${alertId}">
+        <a class="close" data-dismiss="alert" title="${locale.lbl('core.interface.close')}">
+          <i class="fa fa-close"></i>
+          <span class="hide">${locale.lbl('core.interface.close')}</span>
+        </a> ${message}
+    </div>`
     )
 
     if (time !== undefined && time !== null) {
