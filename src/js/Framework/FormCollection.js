8bf3ae5f0760eeb742389cb1e8b009e4d218af01
Bruno Vitorino <bruno@sumocoders.be>
2019-05-08 16:15:51 +0200

    Add extra buttons and render tooltips

--- src/js/Framework/FormCollection.js
+++ src/js/Framework/FormCollection.js
@@ -1,4 +1,5 @@
-require('symfony-collection/jquery.collection');
+import { Framework } from '../Index';
+import 'symfony-collection/jquery.collection';
 
 export default class FormCollection {
   constructor () {
@@ -17,9 +18,23 @@ export default class FormCollection {
         return;
       }
 
+      const resetTooltips = () => {
+        const tooltips = document.querySelector('.tooltip');
+        if (tooltips !== null) {
+          tooltips.remove();
+        }
+        new Framework().initializeTooltips();
+      };
+
       item.collection({
         add: buttonsContainer.find('[data-role="collection-template-button"][data-action="add"]')[0].outerHTML,
-        remove: buttonsContainer.find('[data-role="collection-template-button"][data-action="remove"]')[0].outerHTML
+        remove: buttonsContainer.find('[data-role="collection-template-button"][data-action="remove"]')[0].outerHTML,
+        up: buttonsContainer.find('[data-role="collection-template-button"][data-action="up"]')[0].outerHTML,
+        down: buttonsContainer.find('[data-role="collection-template-button"][data-action="down"]')[0].outerHTML,
+        after_up: resetTooltips,
+        after_down: resetTooltips,
+        after_add: resetTooltips,
+        after_remove: resetTooltips,
       });
 
       buttonsContainer.remove();
