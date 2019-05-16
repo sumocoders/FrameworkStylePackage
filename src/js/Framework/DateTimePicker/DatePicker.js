b17028a50b34f3b740e4bdcd89a3ae61e8493dbe
Bruno Vitorino <bruno@sumocoders.be>
2019-05-16 16:10:10 +0200

    Add more default options

--- src/js/Framework/DateTimePicker/DatePicker.js
+++ src/js/Framework/DateTimePicker/DatePicker.js
@@ -1,12 +1,23 @@
-import 'tempusdominus-bootstrap-4';
 import 'tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.css'
+import 'tempusdominus-bootstrap-4/build/js/tempusdominus-bootstrap-4';
 
 export default class DatePicker {
   constructor (element) {
     this._element = $(element);
 
     this.options = {
-      format: 'DD/MM/YYYY'
+      format: 'L',
+      icons: {
+        time: 'fas fa-clock',
+        date: 'fas fa-calendar',
+        up: 'fas fa-arrow-up',
+        down: 'fas fa-arrow-down',
+        previous: 'fas fa-chevron-left',
+        next: 'fas fa-chevron-right',
+        today: 'fas fa-calendar-check',
+        clear: 'fas fa-delete',
+        close: 'fas fa-times'
+      }
     };
 
     this.init();
