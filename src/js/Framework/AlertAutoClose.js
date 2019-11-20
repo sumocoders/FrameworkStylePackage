11d6595e4054b89f6b70aaafa204178fabc12054
Sarah Meysman <sarah@sumocoders.be>
2019-11-20 16:32:46 +0100

    Write the close alert js in a method

--- src/js/Framework/AlertAutoClose.js
+++ src/js/Framework/AlertAutoClose.js
@@ -1,5 +1,9 @@
 export class AlertAutoClose {
   constructor () {
+    this.closeAlert()
+  }
+
+  closeAlert () {
     setTimeout(() => {
       $('.alert.alert-success.alert-dismissible.notification').alert('close')
     }, 5000);
