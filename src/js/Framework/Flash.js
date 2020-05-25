d71a38b84c478905ba74d67855b89bb05484ac77
Sarah Meysman <sarah@sumocoders.be>
2020-05-25 18:23:25 +0200

    Rewrite the flash messages in the js file

--- src/js/Framework/Flash.js
+++ src/js/Framework/Flash.js
@@ -1,27 +1,41 @@
 export class Flash {
-  add (message, type, time) {
+  static add (message, type, delay = 10000) {
+
+    console.log('---- HELLOOWWWWW')
     let alertId = Date.now()
 
+    let icon = 'check'
+
+    if (type == 'info') {
+      icon = 'info-circle'
+    }
+    if (type == 'warning') {
+      icon = 'exclamation'
+    }
+    if (type == 'danger') {
+      icon = 'exclamation-triangle'
+    }
+
     $('.toasts-wrapper').prepend(
-      `<div class="toast toast-${type} fade show" data-alert-id="${alertId}">
+      `<div class="toast toast-${type}" data-alert-id="${alertId}" role="status" aria-live="polite" data-delay="${delay}">
           <div class="toast-body">
             <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">
               <span aria-hidden="true">&times;</span>
             </button>
-            ${message}
+            <div class="d-flex flex-row">
+              <i class="fas fa-${icon} mr-3 mt-1 toast-icon"></i>
+              ${message}
+            </div>
          </div>
     </div>`
     )
 
-    if (time !== undefined && time !== null) {
-      let callback = () => this.remove(alertId)
-      setTimeout(callback, time)
-    }
+    $('[data-alert-id=' + alertId + ']').toast('show')
 
     return alertId
   }
 
-  remove (alertId) {
-    $('[data-alert-id=' + alertId + ']').alert('close')
+  static remove (alertId) {
+    $('[data-alert-id=' + alertId + ']').toast('hide')
   }
 }
