e4d29679a4ed6844d1c33d479a48e494cac1245c
Sander De la Marche <sander@sumocoders.be>
2021-11-17 11:55:05 +0100

    Drop more deprecated modules

--- src/js/Framework/DragAndDrop.js
+++
@@ -1,9 +0,0 @@
-import Sortable from 'sortablejs'
-
-export class DragAndDrop {
-  constructor (element) {
-    this.element = element
-    this.options = element.dataset.options
-    this.sortable = Sortable.create(this.element, this.options)
-  }
-}
