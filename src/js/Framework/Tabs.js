52ac6a932946b9b67e39084441ca435934e96263
Sander De la Marche <sander@sumocoders.be>
2022-05-11 15:33:56 +0200

    Remove obsolete tab changing code, we use data-attributes now

--- src/js/Framework/Tabs.js
+++ src/js/Framework/Tabs.js
@@ -1,28 +1,6 @@
 export class Tabs {
   constructor () {
-    this.initEventListeners()
     this.loadTab()
-  }
-
-  initEventListeners () {
-    document.querySelectorAll('.nav-tabs a').forEach((link) => {
-      link.addEventListener('click', this.changeTab)
-    })
-  }
-
-  changeTab (event) {
-    /* if the browser supports history.pushState(), use it to update the URL
-     with the fragment identifier, without triggering a scroll/jump */
-    if (window.history && window.history.pushState) {
-      /* an empty state object for now — either we implement a proper
-       popstate handler ourselves, or wait for jQuery UI upstream */
-      window.history.pushState({}, document.title, event.currentTarget.getAttribute('href'))
-    } else {
-      const scrolled = document.body.scrollTop
-      window.location.hash = '#' + event.currentTarget.getAttribute('href').split('#')[1]
-      document.body.scrollTop = scrolled
-    }
-    event.currentTarget.tab('show')
   }
 
   loadTab () {
