a196561947fbed211b5a47d5a3b95190bbaf2245
Sander De la Marche <sander@sumocoders.be>
2022-05-20 14:14:05 +0200

    Switch the default Select component to Tom Select

--- src/js/Framework/Select.js
+++ src/js/Framework/Select.js
@@ -1,18 +1,24 @@
-import { PluginNotFound } from '../Exception/PluginNotFound'
+import TomSelect from 'tom-select'
 
 export class Select {
   constructor (element) {
-    if (!$.isFunction($.fn.select2)) {
-      throw new PluginNotFound('Select2')
-    }
-
     this.element = element
+
     this.initSelect()
   }
 
   initSelect () {
-    this.element.select2({
-      theme: 'bootstrap-5'
+    this.select = new TomSelect(this.element, {
+      persist: false,
+      create: create,
+      render: {
+        no_results: function (data, escape) {
+          return '<div class="no-results">Geen resultaten</div>'
+        },
+        option_create: function (data, escape) {
+          return '<div class="create">Voeg <strong>' + escape(data.input) + '</strong> toe&hellip;</div>'
+        }
+      }
     })
   }
 }
