9b1775546cfa76287cde3acde2d95448ce4a1a10
Sander De la Marche <sander@sumocoders.be>
2023-04-11 13:55:19 +0200

    Rely on the Dutch i18n offered by Flatpickr instead of manually translating the datepicker widgets

--- src/js/Framework/DateTimePicker/TimePicker.js
+++ src/js/Framework/DateTimePicker/TimePicker.js
@@ -1,10 +1,12 @@
 import flatpickr from 'flatpickr'
+import { Dutch } from 'flatpickr/dist/l10n/nl.js'
 
 export class TimePicker {
   constructor (element) {
     this.element = flatpickr(element, {
       enableTime: true,
-      noCalendar: true
+      noCalendar: true,
+      locale: Dutch
     })
 
     element.parentNode.querySelector('[data-flatpicker-clear]').addEventListener('click', event => element._flatpickr.clear())
