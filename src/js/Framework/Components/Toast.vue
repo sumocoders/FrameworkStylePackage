58950d835a90bcadb54dc73027f6216c5e44203f
Daphne Slootmans <daphne@sumocoders.be>
2023-09-12 16:08:40 +0200

    Add toast progress bar and update styling

--- src/js/Framework/Components/Toast.vue
+++ src/js/Framework/Components/Toast.vue
@@ -10,76 +10,80 @@
     :data-bs-autohide="autoHideState"
     :id="id">
     <div class="toast-body">
-      <div class="d-flex flex-row">
-        <i
-          :class="[
+      <div class="d-flex flex-row align-items-center">
+        <div class="toast-icon-wrapper me-3">
+          <i
+              :class="[
             type ? 'success' : '','fas fa-check',
             type ? 'danger' : '','fas fa-exclamation-triangle',
             type ? 'info' : '','fas fa-info-circle',
             type ? 'warning' : '','fas fa-exclamation',
           ]"
-          class="me-3 mt-1 toast-icon"></i>
+              class="toast-icon"></i>
+        </div>
         <span v-html="message"></span>
       </div>
       <button type="button" class="btn-close ms-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-progress" v-if="autoHideState === 'true'">
+      <div class="toast-progress--inner" :style="`animation-duration:${delay}ms`"></div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'toast',
-    props: {
-      type: {
-        type: String,
-        default: 'success'
-      },
-      delay: {
-        type: Number,
-        default: 10000
-      },
-      message: String,
-      autoHide: {
-        type: String,
-        default: 'null'
-      },
-      id: {
-        type: String,
-        default: 'toast' + Date.now()
-      }
+export default {
+  name: 'toast',
+  props: {
+    type: {
+      type: String,
+      default: 'success'
     },
-    methods: {
-      showToast() {
-        const toast = new window.bootstrap.Toast(this.$el)
-        toast.show()
-      },
+    delay: {
+      type: Number,
+      default: 10000
     },
-    computed: {
-      role: function() {
-        if (this.type === 'danger') {
-          return 'alert';
-        }
-        return 'status'
-      },
-      live: function() {
-        if (this.type === 'danger') {
-          return 'assertive';
-        }
-        return 'polite'
-      },
-      autoHideState: function() {
-        if (this.autoHide !== 'null') {
-          return this.autoHide
-        }
-        if (this.type === 'danger') {
-          return 'false'
-        }
-        return 'true'
-      }
+    message: String,
+    autoHide: {
+      type: String,
+      default: 'null'
     },
-    mounted () {
-      this.showToast()
+    id: {
+      type: String,
+      default: 'toast' + Date.now()
     }
-  };
+  },
+  methods: {
+    showToast () {
+      const toast = new window.bootstrap.Toast(this.$el)
+      toast.show()
+    },
+  },
+  computed: {
+    role: function () {
+      if (this.type === 'danger') {
+        return 'alert'
+      }
+      return 'status'
+    },
+    live: function () {
+      if (this.type === 'danger') {
+        return 'assertive'
+      }
+      return 'polite'
+    },
+    autoHideState: function () {
+      if (this.autoHide !== 'null') {
+        return this.autoHide
+      }
+      if (this.type === 'danger') {
+        return 'false'
+      }
+      return 'true'
+    }
+  },
+  mounted () {
+    this.showToast()
+  }
+}
 </script>
-
