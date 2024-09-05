9c9ab7891a5788298d2ceae051796290047cd324
Daphne Slootmans <daphne@sumocoders.be>
2024-09-05 15:36:34 +0200

    Refactor flatpickr locales to use import

--- src/js/Framework/DateTimePicker/DatePicker.js
+++ src/js/Framework/DateTimePicker/DatePicker.js
@@ -1,4 +1,24 @@
 import flatpickr from 'flatpickr'
+// import the most common languages
+import { Austria } from 'flatpickr/dist/l10n/at.js'
+import { Czech } from 'flatpickr/dist/l10n/cs.js'
+import { Danish } from 'flatpickr/dist/l10n/da.js'
+import { Dutch } from 'flatpickr/dist/l10n/nl.js'
+import { Estonian } from 'flatpickr/dist/l10n/et.js'
+import { Finnish } from 'flatpickr/dist/l10n/fi.js'
+import { French } from 'flatpickr/dist/l10n/fr.js'
+import { German } from 'flatpickr/dist/l10n/de.js'
+import { Greek } from 'flatpickr/dist/l10n/gr.js'
+import { Latvian } from 'flatpickr/dist/l10n/lv.js'
+import { Lithuanian } from 'flatpickr/dist/l10n/lt.js'
+import { Italian } from 'flatpickr/dist/l10n/it.js'
+import { Norwegian } from 'flatpickr/dist/l10n/no.js'
+import { Polish } from 'flatpickr/dist/l10n/pl.js'
+import { Portuguese } from 'flatpickr/dist/l10n/pt.js'
+import { Slovak } from 'flatpickr/dist/l10n/sk.js'
+import { Swedish } from 'flatpickr/dist/l10n/sv.js'
+import { Spanish } from 'flatpickr/dist/l10n/es.js'
+import { Slovenian } from 'flatpickr/dist/l10n/sl.js'
 
 export class DatePicker {
   constructor (element) {
@@ -8,10 +28,8 @@ export class DatePicker {
     }
 
     try {
-      const i18n = require('flatpickr/dist/l10n/' + locale + '.js').default[locale]
-
       this.element = flatpickr(element, {
-        locale: i18n
+        locale: locale
       })
     } catch (ex) {
       console.log('No translation found for ' + locale)
