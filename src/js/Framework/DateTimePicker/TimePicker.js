313010baed489ba1ea8512444265d4c90c9437ab
Jonas De Keukelaere <jonas@sumocoders.be>
2024-10-01 15:02:21 +0100

    Fix flatpickr element

--- src/js/Framework/DateTimePicker/TimePicker.js
+++ src/js/Framework/DateTimePicker/TimePicker.js
@@ -7,6 +7,6 @@ export class TimePicker extends DatePicker {
     this.element._flatpickr.config.enableTime = true
     this.element._flatpickr.config.noCalendar = true
 
-    element.parentNode.querySelector('[data-flatpicker-clear]').addEventListener('click', event => element._flatpickr.clear())
+    element.parentNode.querySelector('[data-flatpicker-clear]').addEventListener('click', event => this.element._flatpickr.clear())
   }
 }
