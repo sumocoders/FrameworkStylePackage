0c3133a7c5ea234192c0409965714abeb5f330d9
Jonas De Keukelaere <jonas@sumocoders.be>
2024-10-02 07:15:05 +0100

    Fix time pickers

--- src/js/Framework/DateTimePicker/DateTimePicker.js
+++ src/js/Framework/DateTimePicker/DateTimePicker.js
@@ -2,8 +2,6 @@ import { DatePicker } from './DatePicker.js'
 
 export class DateTimePicker extends DatePicker {
   constructor (element) {
-    super(element)
-
-    this.element._flatpickr.config.enableTime = true
+    super(element, true)
   }
 }
